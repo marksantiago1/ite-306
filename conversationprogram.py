b413a215b427c91b08d31ca6e458335becc70204
marksantiago1 <115474677+marksantiago1@users.noreply.github.com>
2023-07-20 14:09:25 +0800

    Add files via upload

---
+++ conversationprogram.py
@@ -0,0 +1,14 @@
+gram = int(input("Enter gram: "))
+KG = gram / 1000
+MilliG = gram * 1000
+MicroG = gram * 1000000
+Pound = gram * 453.6
+Ounce = gram / 28.35
+Liter = gram / 1000
+
+print(gram, " gram to Kilogram = ", KG)
+print(gram, " gram to Milligram = ", MilliG)
+print(gram, " gram to Microgram = ", MicroG)
+print(gram, " gram to Pound = ", Pound)
+print(gram, " gram to Ounce = ", Ounce)
+print(gram, " gram to Liter = ", Liter)
